586cb67dfd677fe47f5636d01adac423a121405b
devlua <bkwry8128@gmail.com>
2016-09-11 14:49:03 +0000

    Update HELLO.lua

--- plugins/HELLO.lua
+++ plugins/HELLO.lua
@@ -1,52 +1,52 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
+▀▄ ▄▀   BY SAJJADHUSSIEN   (@DEV_PRO  ▀▄ ▄▀ 
+▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN      ▀▄ ▄▀ 
+▀▄ ▄▀        wolcom   :  ترحيب             ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
+--]] 
 
-do
-    
-local function axis(msg,matches)
+do 
+local function axis(msg,matches) 
     if matches[1] == "chat_add_user"  then 
-     local text = 'اهلا 😊👋 بك عزيزي في المجموعة 👥🔕'..'\n'..'\n'
-     ..'المعلومات 📋 الخاصة بك 🔷'..'\n'
-    ..'❇ اسمك :  '..msg.action.user.print_name..'\n'
-    ..'❇ معرفك : @'..(msg.action.user.username or "لا يوجد")..'\n'
-    ..'❇ الايدي : '..msg.action.user.id..'\n'
-    ..'❇رقم الهاتف : '..(msg.action.user.phone or "لا يوجد")..'\n'
-    ..'♦🔹♦🔸♦🔹♦ـ'..'\n'
-    ..'❇ اسم المجموعة : '..msg.to.title..'\n'
-    ..'❇ ايدي المجموعة : '..msg.to.id..'\n'
-    ..'♦🔹♦🔸♦🔹♦ـ'..'\n'
-    ..'❇ ضافك : '..msg.from.print_name..'\n'
-    ..'❇ معرفة : @'..(msg.from.username or "لا يوجد")..'\n'
-..' ايدية ❇ : '..msg.from.id..'\n'
-    ..'❇ رقم هاتفةة : '..(msg.from.phone or "لا يوجد")..'\n'
-    ..'♦🔹♦🔸♦🔸♦ـ'..'\n'
-    ..'📅 التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp)
-    ..'🕚 الوقت : '..os.date(' %T*', os.time())..'\n'  
-     ..'♦🔹♦🔸♦🔹♦ـ'..'\n'
-    ..'❇ معرف القناة : @DEV_PRO'..'\n'..'\n'
-     return reply_msg(msg.id, text, ok_cb, false)
-     end
-    if matches[1] == "chat_add_user_link" then
-        local text = 'اهلا 😊👋 بك عزيزي في المجموعة 👥🔕'..'\n'..'\n'
-     ..'المعلومات 📋 الخاصة بك 🔷'..'\n'
-    ..'❇ اسمك :  '..msg.from.print_name..'\n'
-    ..'❇ معرفك : @'..(msg.from.username or "لا يوجد")..'\n'
-    ..'❇ الايدي : '..msg.from.id..'\n'
-    ..'❇رقم الهاتف : '..(msg.from.phone or "لا يوجد")..'\n'
-    ..'♦🔹♦🔸♦🔹♦ـ'..'\n'
-    ..'❇ اسم المجموعة : '..msg.to.title..'\n'
-    ..'❇ ايدي المجموعة : '..msg.to.id..'\n'
-    ..'♦🔹♦🔸♦🔹♦ـ'..'\n'
-    ..'📅 التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp)
-    ..'🕚 الوقت : '..os.date(' %T*', os.time())..'\n'  
-     ..'♦🔹♦🔸♦🔹♦ـ'..'\n'
-    ..'❇ قناة البوت  : @DEV_PRO'..'\n'
-        return reply_msg(msg.id, text, ok_cb, false)
-  end
-end
-return {
-    patterns = {
-        "^!!tgservice (chat_add_user)$",
-        "^!!tgservice (chat_add_user_link)$",
-    },
- run = run
-}
+     local text = ' اهلا بك في المجموعه كبد حياتي 🙈💋'..'\n'..'\n' 
+    ..'❇ اسمك : '..msg.action.user.print_name..'\n' 
+    ..'❇ معرفك : @'..(msg.action.user.username or "لا يوجد")..'\n' 
+    ..'❇ الايدي : '..msg.action.user.id..'\n' 
+    ..'❇رقم هاتفك : '..(msg.action.user.phone or "لا يوجد")..'\n' 
+    ..'❇الي ضافك : '..msg.from.print_name..'\n' 
+    ..'❇ معرف الي ضافك : @'..(msg.from.username or "لا يوجد")..'\n' 
+    ..'❇ التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp) 
+    ..'❇ الوقت : '..os.date(' %T*', os.time())..'\n' 
+
+     return reply_msg(msg.id, text, ok_cb, false) 
+ end 
+
+    if matches[1] == "chat_add_user_link" then 
+      local text = ' اهلا بك في المجموعه كبد حياتي 🙈💋'..'\n'..'\n' 
+     ..'❇ اسمك :  '..msg.from.print_name..'\n' 
+    ..'❇ معرفك : @'..(msg.from.username or "لا يوجد")..'\n' 
+    ..'❇ الايدي : '..msg.from.id..'\n' 
+    ..'❇رقم هاتفك : '..(msg.from.phone or "لا يوجد")..'\n' 
+    ..'❇ اسم المجموعة : '..msg.to.title..'\n' 
+    ..'❇ ايدي المجموعة : '..msg.to.id..'\n' 
+    ..'❇ التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp) 
+    ..'❇ الوقت : '..os.date(' %T*', os.time())..'\n' 
+
+        return reply_msg(msg.id, text, ok_cb, false) 
+  end 
+      if matches[1] == "chat_del_user" then 
+       return " 🔸 الله وياك/ج كبد عمري 😍🙈 لتنسه الباب مفتوح 🌚"..msg.action.user.first_name 
+end 
+end 
+return { 
+    patterns = { 
+        "^!!tgservice (chat_add_user)$", 
+        "^!!tgservice (chat_add_user_link)$", 
+        "^!!tgservice (chat_del_user)$" 
+    }, 
+ run = axis 
+} 
 end
