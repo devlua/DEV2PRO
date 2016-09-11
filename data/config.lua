34d7bb448fd2ff6a19bfde0b6479f6538c7227a3
devlua <bkwry8128@gmail.com>
2016-09-11 17:04:08 +0000

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,54 @@
+do local _ = {
+  about_text = "Welcome to DEV2PRO For more information Subscribe to the channel @DEV_PRO ",
+  enabled_plugins = {
+    "HELLO",
+    "TEAM",
+    "addsudo",
+    "all",
+    "ar-aint-spam",
+    "ar-help",
+    "ar-help1",
+    "ar-lock-bot",
+    "ar-lock-fwd",
+    "ar-mad",
+    "ar-msg-check",
+    "ar-onservice",
+    "ar-owbers",
+    "ar-sudo",
+    "ar_help3",
+    "arabic-lock",
+    "badword",
+    "banhmmer",
+    "devs",
+    "get",
+    "getfile",
+    "help2",
+    "info",
+    "invite",
+    "ipex",
+    "leven-ban",
+    "love",
+    "love2",
+    "me",
+    "nowgroup",
+    "pulgins",
+    "redis",
+    "replay",
+    "run",
+    "serverinfo",
+    "sueprgroup",
+  },
+  help_text = "",
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    241857359,
+   
+    
+  }
+}
+return _
+end
